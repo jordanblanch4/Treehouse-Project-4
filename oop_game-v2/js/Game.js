ac9ea50c46fb043a94e2aabc4c74dd1417442dc8
Jordan Blanchard <jordanb17b@icloud.com>
2020-04-19 10:32:37 -0700

    starter files and basic classes

---
+++ oop_game-v2/js/Game.js
@@ -0,0 +1,13 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Game.js */
+
+ class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [`Geez Louise`,`Party Hardy`,`small computer book club`,`Good morning, Vietnam`,`Air trip`];
+        this.activePhrase = null;
+
+    }
+
+ }
